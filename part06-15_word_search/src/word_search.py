4e6048ec0858037dd85b40789a48d16bfeeecf35
me <67919144+FBaia2@users.noreply.github.com>
2024-03-03 16:04:05 +0000

    word_search, writing part 6

--- part06-15_word_search/src/word_search.py
+++ part06-15_word_search/src/word_search.py
@@ -1,1 +1,34 @@
-# Write your solution here
+def find_words(search_term: str):
+    count_asterisk = 0
+    if "." not in search_term or "*" not in search_term:
+        with open("words.txt") as my_file:
+            for line in my_file:
+                # line.replace("\n", "")
+                if search_term in line:
+                    print(line)
+    elif "*" in search_term:
+        for i in search_term:
+            if "*" in search_term:
+                count_asterisk +=1
+                if count_asterisk > 1:
+                    print("you can't have more then one asterisk")
+                else:
+                    if search_term[0] == "*":
+                    elif search_term[-1] == "*":
+
+
+    elif "." in search_term:
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__": 
+    print(find_words("vokes"))
