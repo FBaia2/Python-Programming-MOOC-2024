64c66c9e5a2d521f77abaf62391797153d07f808
me <67919144+FBaia2@users.noreply.github.com>
2024-03-03 16:05:19 +0000

    Writing part6

--- part06-11_diary/src/diary.py
+++ part06-11_diary/src/diary.py
@@ -1,1 +1,16 @@
-# Write your solution here
+while True:
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    txt = input("Function: ")
+    if txt == "0":
+        print("Bye now!")
+        break
+    elif txt == "1":
+        with open("diary.txt", "a") as my_file:
+            deez = input("Diary entry:")
+            my_file.write(deez + "\n")
+            # print(deez, file=my_file)
+            print("Diary saved")
+    elif txt == "2":
+        with open("diary.txt") as my_file:
+            contents = my_file.read()
+            print(contents)
--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def store_personal_data(person: tuple):
+    with open("people.csv", "a") as my_file:
+        people0 = str(person[0])
+        people1 = str(person[1])
+        people2 = str(person[2])
+        people = people0 + ";" + people1 + ";" + people2
+        my_file.write(people)
+        my_file.write("\n")
+
+
+
+
+
+if __name__=="__main__":
+    person_data = ("Paul Paulson", 37, 175.5)  # This is a tuple
+    store_personal_data(person_data)
