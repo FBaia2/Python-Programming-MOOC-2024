278e27d304893ba18e2ad66c635887edcfe443fa
me <67919144+FBaia2@users.noreply.github.com>
2024-03-04 14:18:58 +0000

    sf

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,19 @@
-# Write your solution here
+def new_person(name: str, age: int):
+    # Check if name is an empty string
+    if not name:
+        raise ValueError("Name cannot be an empty string.")
+    # Check if name contains less than two words
+    if len(name.split()) < 2:
+        raise ValueError("Name must contain at least two words.")
+    # Check if name is longer than 40 characters
+    if len(name) > 40:
+        raise ValueError("Name cannot be longer than 40 characters.")
+    # Check if age is a negative number or greater than 150
+    if age < 0 or age > 150:
+        raise ValueError("Age must be a positive number and less than or equal to 150.")
+    # If all checks pass, return the tuple
+    return (name, age)
+
+if __name__=="__main__":
+    n = new_person("Jonh deeee" , 30)
+    print(n)
