82d314fff0133f0eedfddeaf1adf5d0ad7913331
me <67919144+FBaia2@users.noreply.github.com>
2024-03-04 12:23:58 +0000

    w

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -2,8 +2,9 @@ def read_input(cmd: str, x : int, y: int):
     while True:
         num = input(cmd)
         if  num.isdigit():
-            if num >= x and num <= y:
-                return num
+            numero = int(num)
+            if numero >= x and numero <= y:
+                return numero
             else:
                 print(f"You must type in an integer between {x} and {y}")
         else:
@@ -16,14 +17,6 @@ def read_input(cmd: str, x : int, y: int):
 
 
 
-
-
-
-
-
-
-
-
 if __name__== "__main__":
     number = read_input("Please type in a number: ", 5, 10)
-print("You typed in:", number)
+    print("You typed in:", number)
