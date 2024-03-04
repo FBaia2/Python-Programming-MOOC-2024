29db115da166d4916fb76dda54f7348347cd3a3b
me <67919144+FBaia2@users.noreply.github.com>
2024-03-04 11:58:05 +0000

    another one

--- part06-16_dictionary_file/src/dictionary_file.py
+++ part06-16_dictionary_file/src/dictionary_file.py
@@ -1,9 +1,20 @@
-# Write your solution here
+eng_name = ""
+fin_name = ""
 while True:
     print("1 - Add word, 2 - Search, 3 - Quit")
-    func = input(int("Function: "))
-    if func == 3:
+    func = input("Function: ")
+    if func == "3":
         print("Bye!")
-    elif func == 1:
-        
-    elif func == 2:
+        break
+    elif func == "1":
+        fin_name = input("The word in Finnish: ")
+        eng_name = input("The word in English: ")
+        print("Dictionary entry added")
+        with open("dictionary.txt", "a") as my_file:
+            my_file.write(f"{fin_name} - {eng_name}\n")
+    elif func == "2":
+        search_term = input("Search term: ")
+        with open("dictionary.txt") as my_file:
+            for line in my_file:
+                if search_term in line:
+                    print(line)
--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,29 @@
-# Write your solution here
+def read_input(cmd: str, x : int, y: int):
+    while True:
+        num = input(cmd)
+        if  num.isdigit():
+            if num >= x and num <= y:
+                return num
+            else:
+                print(f"You must type in an integer between {x} and {y}")
+        else:
+            print(f"You must type in an integer between {x} and {y}")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+print("You typed in:", number)
