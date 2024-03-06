4b549ee24126fb798e958a4f007fa13deabe9886
me <67919144+FBaia2@users.noreply.github.com>
2024-03-06 14:05:42 +0000

    finished the modules section

--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,25 @@
-# Write your solution here
+import string
+
+def separate_characters(my_string: str):
+    this1 = ""
+    this2 = ""
+    this3 = ""
+
+    thisis1 = string.ascii_letters
+    thisis2 = string.punctuation
+
+    for letter in my_string:
+        if letter in thisis1:
+            this1 += letter
+        elif letter in thisis2:
+            this2 += letter
+        else:
+            this3 += letter
+
+    return this1, this2, this3
+
+if __name__ == "__main__":
+    parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+    print(parts[0])  # Letters
+    print(parts[1])  # Punctuation
+    print(parts[2])  # Other characters
--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,19 @@
-# Write your solution here
+from fractions import Fraction
+
+def fractionate(amount: int):
+    # Create an empty list to store the fractions
+    fractions = []
+
+    # Loop for the number of parts
+    for _ in range(amount):
+        # Create a fraction representing 1/amount
+        fraction = Fraction(1, amount)
+
+        # Add the fraction to the list
+        fractions.append(fraction)
+
+    # Return the list of fractions
+    return fractions
+
+if __name__ == "__main__" :
+    print(fractionate(5))
