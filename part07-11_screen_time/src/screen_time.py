6695fd63f2666ee04d4f26056ece240c4fdb91c4
me <67919144+FBaia2@users.noreply.github.com>
2024-03-08 01:01:25 +0000

    f

--- part07-11_screen_time/src/screen_time.py
+++ part07-11_screen_time/src/screen_time.py
@@ -1,18 +1,45 @@
-from datetime import date
+from datetime import date, datetime, timedelta  # Import timedelta
+
 filename = input("Filename: ")
-with open(filename, "a") as new_file:
+dates = []
+times = []
+loners = []
+liners = []
+counter = 0
+counting = 0
+with open(filename, "w") as my_file:
     starting_date = input("Starting date: ")
     days = int(input("How many days: "))
     print("Please type in screen time in minutes on each day (TV computer mobile):")
     starting_date = starting_date.split(".")
-    print(starting_date)
-    deez = date(int(starting_date[0]), int(starting_date[1]), int(starting_date[2]))
-    print(deez)
+    deez = date(int(starting_date[2]), int(starting_date[1]), int(starting_date[0]))
     for _ in range(days):
-        screen = input(f"Screen time {starting_date}: ")
+        deez1 = datetime.strftime(deez, "%d.%m.%Y")
+        screen = input(f"Screen time {deez1}: ")
+        dates.append(deez1)
+        times.append(screen)
+        deez = deez + timedelta(days=1)
+     
+    for line in times:
+        line = line.split(" ")
+        liners.append(line)
+    
+    for i in liners:
+        for numba in i:
+            numba = int(numba)
+            counting += numba
+
+    average = float(counting) / float(days)
 
 
-
-
+    my_file.write(f"Time period: {dates[0]}-{dates[-1]}\n")
+    my_file.write(f"Total minutes: {counting}\n")
+    my_file.write(f"Average minutes: {average}\n")
+    for line in times:
+        line = line.replace(" ", "/")
+        loners.append(line)
+    for _ in range(days):
+        my_file.write(f"{dates[counter]}: {loners[counter]}\n")
+        counter +=1
 
     print("Data stored in file late_june.txt")
--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,27 @@
-# Write your solution here
+import json
+def print_persons(filename: str):
+    lista = []
+    counter = 0
+    with open(filename) as my_file:
+        data = my_file.read()
+    courses = json.loads(data)
+    for course in courses:
+        print(course["name"], course["age"], end = "")
+        print(" years", end = " ")
+        for i in course["hobbies"]:
+            lista.append(i)
+        print("(", end= "")
+        for i in lista:
+            counter +=1
+            print(f"{i}", end= "")
+            if counter != len(lista):
+                print(", ", end= "")
+        print(")", end= "")
+        lista = []
+        print("\n")
+        counter = 0
+    
+
+
+if __name__ =="__main__":
+    print_persons("file1.json")
