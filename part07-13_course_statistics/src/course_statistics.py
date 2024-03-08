fc92f163ca1307d4f9759c79a10212b79df06602
me <67919144+FBaia2@users.noreply.github.com>
2024-03-08 13:09:43 +0000

    f

--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,17 @@
-# Write your solution here
+import urllib.request
+import json
+
+def retrieve_all():
+    with urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses") as url:
+        data = json.loads(url.read().decode())
+    print("[")
+    for course in data:
+        if isinstance(course, dict) and 'fullName' in course and 'name' in course and 'year' in course and 'exercises' in course:
+            sum_exercises = sum(course['exercises'])
+            print("    ",(course['fullName'], course['name'], course['year'], sum_exercises))
+        else:
+            print("Course is not a dictionary or does not have the expected keys.")
+    print("]")
+
+if __name__=="__main__":
+    retrieve_all()
