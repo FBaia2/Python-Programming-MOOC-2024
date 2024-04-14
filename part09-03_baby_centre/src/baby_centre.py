caadd6400e8c9bae90610965eff56a897fc84a4e
FBaia2 <squadbaia@gmail.com>
2024-04-14 10:02:17 +0100

    why so ez

--- part09-03_baby_centre/src/baby_centre.py
+++ part09-03_baby_centre/src/baby_centre.py
@@ -1,7 +1,3 @@
-
-# WRITE YOUR SOLUTION HERE:
-#Note! Do not change the class Person!
-
 class Person:
     def __init__(self, name: str, age: int, height: int, weight: int):
         self.name = name
@@ -14,5 +10,31 @@ class BabyCentre:
         self.number_of_weigh_ins = 0
 
     def weigh(self, person: Person):
-        # return the weight of the person passed as an argument
-        return -1
+        self.number_of_weigh_ins += 1
+        return person.weight
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
+
+    def feed(self, person: Person):
+        person.weight += 1  
+
+if __name__ == "__main__":
+    baby_centre = BabyCentre()
+
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
